06d73115ec4816df2ae3c85bea5657989fa2f0ff
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-15 14:14:09 +0200

    init commit

---
+++ patia_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dawid/Pafnutij/patia_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/dawid/adr-i-/adr1_ws/devel;/home/dawid/peps/peps_ws/devel;/opt/ros/kinetic".split(';') if "/home/dawid/adr-i-/adr1_ws/devel;/home/dawid/peps/peps_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ patia_ws/build/uadr_launches/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dawid/Pafnutij/patia_ws/src/uadr_launches
+# Build directory: /home/dawid/Pafnutij/patia_ws/build/uadr_launches
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
