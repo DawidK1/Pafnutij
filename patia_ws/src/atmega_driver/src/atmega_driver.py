dd521caedf720069208025efff4e1509097ce825
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-16 11:33:57 +0200

    added basic control

---
+++ patia_ws/src/atmega_driver/src/atmega_driver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from smbus import SMBus
+import rospy
+import numpy
+from time import sleep
+
+DEV_ADDR = 0x44
+LEFT_WHEEL_ADDR = 1
+RIGHT_WHEEL_ADDR = 2
+CASTER_WHEEL_ADDR = 3
+
+bus = SMBus(1)
+
+
+def set_wheels(left, right, caster):
+    l_dir = 0
+    r_dir = 0
+    if left < 0:
+        l_dir = 1
+    if right < 0:
+        r_dir = 1
+
+    bus.write_byte_data(DEV_ADDR, LEFT_WHEEL_ADDR, abs(int(left*31)) + l_dir)
+    bus.write_byte_data(DEV_ADDR, RIGHT_WHEEL_ADDR, abs(int(right*31)) + r_dir)
+    bus.write_byte_data(DEV_ADDR, CASTER_WHEEL_ADDR, (-caster + 1)*255)
+
+for i in numpy.linspace(-1, 1, 60):
+    set_wheels(i, i, i)
+    sleep(1.0)
