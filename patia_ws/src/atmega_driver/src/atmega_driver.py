8f2359bb6eb5843da46921f661ebf5b98378d850
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-16 11:38:11 +0200

    bug fix

--- patia_ws/src/atmega_driver/src/atmega_driver.py
+++ patia_ws/src/atmega_driver/src/atmega_driver.py
@@ -21,8 +21,8 @@ def set_wheels(left, right, caster):
     if right < 0:
         r_dir = 1
 
-    bus.write_byte_data(DEV_ADDR, LEFT_WHEEL_ADDR, abs(int(left*31)) + l_dir)
-    bus.write_byte_data(DEV_ADDR, RIGHT_WHEEL_ADDR, abs(int(right*31)) + r_dir)
+    bus.write_byte_data(DEV_ADDR, LEFT_WHEEL_ADDR, abs(int(left*16)*2) + l_dir)
+    bus.write_byte_data(DEV_ADDR, RIGHT_WHEEL_ADDR, abs(int(right*16)*2) + r_dir)
     bus.write_byte_data(DEV_ADDR, CASTER_WHEEL_ADDR, int((-caster + 1)*255))
 
 for i in numpy.linspace(-1, 1, 60):
