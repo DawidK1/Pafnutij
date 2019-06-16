1b3f94cd74b530670575ac3651046761d05948bf
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-16 16:15:54 +0200

    fix

--- patia_ws/build/catkin_generated/order_packages.py
+++ patia_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/dawid/Pafnutij/patia_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/dawid/adr-i-/adr1_ws/devel;/home/dawid/aquilo/workspace/devel;/opt/ros/kinetic".split(';') if "/home/dawid/adr-i-/adr1_ws/devel;/home/dawid/aquilo/workspace/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/dawid/Pafnutij/patia_ws/devel;/opt/ros/kinetic".split(';') if "/home/dawid/Pafnutij/patia_ws/devel;/opt/ros/kinetic" != "" else []
