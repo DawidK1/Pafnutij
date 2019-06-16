8959f8967a95ff6aeae4307962cf01e10ddca435
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-16 12:14:31 +0200

    test

--- patia_ws/src/atmega_driver/src/atmega_driver.py
+++ patia_ws/src/atmega_driver/src/atmega_driver.py
@@ -13,18 +13,31 @@ CASTER_WHEEL_ADDR = 3
 bus = SMBus(1)
 
 
+def saturate(a):
+    if a > 1:
+        return 1
+    elif a < -1:
+        return -1
+    return a
+
+
 def set_wheels(left, right, caster):
-    l_dir = 0
-    r_dir = 0
+    l_dir = 1
+    r_dir = 1
     if left < 0:
-        l_dir = 1
+        l_dir = 0
     if right < 0:
-        r_dir = 1
+        r_dir = 0
+    caster += 0.03
+
+    caster = saturate(caster)
+    left = saturate(left)
+    right = saturate(right)
 
     bus.write_byte_data(DEV_ADDR, LEFT_WHEEL_ADDR, abs(int(left*16)*2) + l_dir)
     bus.write_byte_data(DEV_ADDR, RIGHT_WHEEL_ADDR,
                         abs(int(right*16)*2) + r_dir)
-    bus.write_byte_data(DEV_ADDR, CASTER_WHEEL_ADDR, int((-caster + 1)*128))
+    bus.write_byte_data(DEV_ADDR, CASTER_WHEEL_ADDR, int((-caster + 1.0)*128))
 
 
 # for i in numpy.linspace(-1, 1, 60):
