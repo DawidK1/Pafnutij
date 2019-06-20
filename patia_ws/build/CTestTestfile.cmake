fca0a4bdda578cf4c19abbea33916a20d164b68b
Dawid Kasieczka <dawid.kasieczka@gmail.com>
2019-06-20 16:34:52 +0200

    control commit

--- patia_ws/build/CTestTestfile.cmake
+++ patia_ws/build/CTestTestfile.cmake
@@ -6,4 +6,5 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
 subdirs(atmega_driver)
+subdirs(patia_logic)
 subdirs(uadr_launches)
--- patia_ws/build/catkin_generated/setup_cached.sh
+++ patia_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/arm-linux-gnueabihf"
 export PATH="/opt/ros/kinetic/bin:/home/dawid/bin:/home/dawid/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/arm-linux-gnueabihf/pkgconfig"
 export PWD="/home/dawid/Pafnutij/patia_ws/build"
